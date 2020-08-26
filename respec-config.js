6b80d9982b478290f87670567e71989cbc92ed72
Dominique Hazael-Massieux <dom@w3.org>
2020-08-26 11:11:19 +0200

    Clean up respec autolinks and other markup

--- respec-config.js
+++ respec-config.js
@@ -27,6 +27,7 @@ var respecConfig = {
       ]
     }
   ],
+  xref: ["webrtc", "html", "webidl"],
   localBiblio: {
     "IANA-STUN-6": {
       "title": "STUN Error Codes",
@@ -57,78 +58,6 @@ var respecConfig = {
       "status": "13 August 2019. Internet Draft (work in progress)",
       "publisher": "IETF"
     },
-   "RFC6544": {
-      "title": "TCP Candidates with Interactive Connectivity Establishment (ICE)",
-      "href": "https://tools.ietf.org/html/rfc6544",
-      "authors": [
-        "J. Rosenberg",
-        "A. Keranen",
-        "B. B. Lowekamp",
-        "A. B. Roach"
-      ],
-      "status": "March 2012. RFC",
-      "publisher": "IETF"
-    },
-   "RFC7064": {
-      "title": "URI Scheme for Session Traversal Utilities for NAT (STUN) Protocol",
-      "href": "https://tools.ietf.org/html/rfc7064",
-      "authors": [
-        "S. Nandakumar",
-        "G. Salgueiro",
-        "P. Jones",
-        "M. Petit-Huguenin"
-      ],
-      "status": "November 2013. RFC",
-      "publisher": "IETF"
-    },
-    "RFC7065": {
-      "title": "Traversal Using Relays around NAT (TURN) Uniform Resource Identifiers",
-      "href": "https://tools.ietf.org/html/rfc7065",
-      "authors": [
-        "M. Petit-Huguenin",
-        "S. Nandakumar",
-        "G. Salgueiro",
-        "P. Jones"
-      ],
-      "status": "November 2013. RFC",
-      "publisher": "IETF"
-    },
-   "RFC7635": {
-      "title": "Session Traversal Utlities for NAT (STUN) Extension for Third Party Authorization",
-      "href": "https://tools.ietf.org/html/rfc7635",
-      "authors": [
-        "T. Reddy",
-        "P. Patil",
-        "R. Ravindranath",
-        "J. Uberti"
-      ],
-      "status": "August 2015. RFC",
-      "publisher": "IETF"
-    },
-   "RFC7675": {
-      "title": "Session Traversal Utilities for NAT (STUN) Usage for Consent Freshness",
-      "href": "https://tools.ietf.org/html/rfc7675",
-      "authors": [
-        "M. Perumal",
-        "D. Wing",
-        "R. Ravindranath",
-        "T. Reddy",
-        "M. Thomson"
-      ],
-      "status": "October 2015. RFC",
-      "publisher": "IETF"
-    },
-   "RFC8445": {
-      "title": "Interactive Connectivity Establishment (ICE): A Protocol for Network Address Translator (NAT) Traversal",
-      "href": "https://tools.ietf.org/html/rfc8445",
-      "authors": [
-        "A. Keranen",
-        "C. Holmberg",
-        "J. Rosenberg"
-      ],
-      "status": "July 2018. RFC",
-      "publisher": "IETF"
-    },
     "RTCWEB-SECURITY-ARCH": {
       "title": "WebRTC Security Architecture",
       "href": "https://tools.ietf.org/html/draft-ietf-rtcweb-security-arch",
@@ -157,16 +86,6 @@ var respecConfig = {
       ],
       "status": "15 April 2018. Internet Draft (work in progress)",
       "publisher": "IETF"
-    },
-    "WEBRTC-STATS": {
-      "title": "Identifiers for WebRTC's Statistics API",
-      "href": "https://w3c.github.io/webrtc-stats/",
-      "authors": [
-        "Harald Alvestrand",
-        "Varun Singh"
-      ],
-      "status": "15 January 2020 (work in progress)",
-      "publisher": "W3C"
     }
   }
 }
